250d54a9991cf47e14bcaffa7b74c313d17f4128
korneliarohulko <korneliarohulko@gmail.com>
2019-05-23 23:27:09 +0200

    Lattice paths

---
+++ 15. Lattice paths/DynamicPathSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class DynamicPathSolution
+    {
+        /* Dynamic programming - better one */
+        static long howManyPath(int n)
+        {
+            long[,] array = new long[n + 1, n + 1];
+            array[0, 0] = 1;
+
+            for(long i = 0; i < n + 1; i++)
+            {
+                for (long j = 0; j < n + 1; j++)
+                {
+                    if (i - 1 >= 0 && j - 1 >= 0)
+                        array[i, j] = array[i - 1, j] + array[i, j - 1];
+                    else if (i - 1 >= 0)
+                        array[i, j] = array[i - 1, j];
+                    else if (j - 1 >= 0)
+                        array[i, j] = array[i, j - 1];
+                }
+            }
+
+            return array[n, n];
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(howManyPath(20));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 15. Lattice paths/RecursivePaths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    /* Lattice paths - recursive solution
+     * Not really effective one.. */
+    class RecursivePaths
+    {        
+        static int howManyPath(int x, int y)
+        {           
+            if (x - 1 >= 0 && y - 1 >= 0)
+                return howManyPath(x - 1, y) + howManyPath(x, y - 1);
+            else if (x - 1 >= 0)
+                return howManyPath(x - 1, y);
+            else if (y - 1 >= 0)
+                return howManyPath(x, y - 1);
+
+            //obvious that x == 0 and y == 0 here
+            return 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(howManyPath(3, 3));
+            Console.ReadKey();
+        }
+    }
+}
